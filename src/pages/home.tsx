1850863574e484838442f0e9581d085b572c06c0
EmersonEcode <emerson.aconprev@outlook.com>
2025-04-14 16:32:55 -0400

    pequenas alterações no header.module.css adicionando pequenos detalhes de responsividade para mobile  index.html adicionando title icon do janela

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,13 +3,16 @@ import { Header } from "../components/Header";
 import styles from '../styles/home.module.css';
 import { Apresentation } from "../components/Apresentation";
 import { AboutMe } from "../components/AboutMe";
-
+import { Skills } from "../components/Skills";
+import {Projects} from '../components/Projects';
 export const Home = () => {
     return (
         <div className={styles.container}>
             <Header/>
             <Apresentation/>
             <AboutMe/>
+            <Skills/>
+            <Projects/>
         </div>
     )
 }
